57cad684475e03a3424cb4edc986c500b9f63a0c
hqlyz <hqlyz112@163.com>
2021-01-22 20:08:08 +0800

    update

--- brute-forcing-rc2/main.go
+++ brute-forcing-rc2/main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/cipher"
 	"encoding/binary"
 	"encoding/hex"
-	"fmt"
 	"log"
 	"regexp"
 	"sync"
@@ -24,18 +23,23 @@ func generate(start, stop uint64, out chan<- *CryptoData, done <-chan struct{}, 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		var (
+			block cipher.Block
+			err   error
+			key   []byte
+			data  *CryptoData
+		)
 		for i := start; i <= stop; i++ {
-			key := make([]byte, 8)
+			key = make([]byte, 8)
 			select {
 			case <-done:
 				return
 			default:
 				binary.BigEndian.PutUint64(key, i)
-				block, err := rc2.New(key[3:], 40)
-				if err != nil {
-					log.Fatalf("create rc2 cipher failed: %v\n", err)
+				if block, err = rc2.New(key[3:], 40); err != nil {
+					log.Fatalln(err)
 				}
-				data := &CryptoData{
+				data = &CryptoData{
 					block: block,
 					key:   key[3:],
 				}
@@ -43,11 +47,13 @@ func generate(start, stop uint64, out chan<- *CryptoData, done <-chan struct{}, 
 			}
 		}
 	}()
+
+	return
 }
 
 func decrypt(ciphertext []byte, in <-chan *CryptoData, done chan struct{}, wg *sync.WaitGroup) {
 	size := rc2.BlockSize
-	plainText := make([]byte, len(ciphertext))
+	plaintext := make([]byte, len(ciphertext))
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -56,11 +62,11 @@ func decrypt(ciphertext []byte, in <-chan *CryptoData, done chan struct{}, wg *s
 			case <-done:
 				return
 			default:
-				data.block.Decrypt(plainText[:size], ciphertext[:size])
-				if numeric.Match(plainText[:size]) {
-					data.block.Decrypt(plainText[size:], ciphertext[size:])
-					if luhn.Valid(string(plainText)) && numeric.Match(plainText[size:]) {
-						fmt.Printf("found %s with key [%x]\n", string(plainText), data.key)
+				data.block.Decrypt(plaintext[:size], ciphertext[:size])
+				if numeric.Match(plaintext[:size]) {
+					data.block.Decrypt(plaintext[size:], ciphertext[size:])
+					if luhn.Valid(string(plaintext)) && numeric.Match(plaintext[size:]) {
+						log.Printf("Card [%s] found using key [%x]\n", plaintext, data.key)
 						close(done)
 						return
 					}
@@ -71,40 +77,49 @@ func decrypt(ciphertext []byte, in <-chan *CryptoData, done chan struct{}, wg *s
 }
 
 func main() {
-	ciphertext, err := hex.DecodeString("0986f2cc1ebdc5c2e25d04a136fa1a6b")
-	if err != nil {
+	var (
+		err        error
+		ciphertext []byte
+	)
+	/*
+	   Assume ECB mode
+	   Assume no padding needed
+	   key        = e612d0bbb6
+	   ciphertext = 0986f2cc1ebdc5c2e25d04a136fa1a6b
+	   plaintext  = 4532651325506680
+	*/
+	if ciphertext, err = hex.DecodeString("0986f2cc1ebdc5c2e25d04a136fa1a6b"); err != nil {
 		log.Fatalln(err)
 	}
 
-	var (
-		min, max, prods    = uint64(0x00000000), uint64(0xffffffff), uint64(75)
-		generateWg, workWg sync.WaitGroup
-		step               = (max - min) / prods
-		start, end         = min, min + step
-		workCount          = 100
-		done               = make(chan struct{})
-		work               = make(chan *CryptoData, workCount)
-	)
+	var prodWg, consWg sync.WaitGroup
+	var min, max, prods = uint64(0x0000000000), uint64(0xffffffffff), uint64(75)
+	var step = (max - min) / prods
 
-	for i := uint64(0); i <= prods; i++ {
+	done := make(chan struct{})
+	work := make(chan *CryptoData, 100)
+	if (step * prods) < max {
+		step += prods
+	}
+	var start, end = min, min + step
+	log.Println("Starting producers...")
+	for i := uint64(0); i < prods; i++ {
 		if end > max {
 			end = max
 		}
-		generate(start, end, work, done, &generateWg)
-
-		start += step
+		generate(start, end, work, done, &prodWg)
 		end += step
+		start += step
 	}
-
 	log.Println("Producers started!")
 	log.Println("Starting consumers...")
-	for i := 0; i < workCount; i++ {
-		decrypt(ciphertext, work, done, &workWg)
+	for i := 0; i < 30; i++ {
+		decrypt(ciphertext, work, done, &consWg)
 	}
 	log.Println("Consumers started!")
 	log.Println("Now we wait...")
-	generateWg.Wait()
+	prodWg.Wait()
 	close(work)
-	workWg.Wait()
+	consWg.Wait()
 	log.Println("Brute-force complete")
 }
